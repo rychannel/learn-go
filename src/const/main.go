b89cf45c0e34bc15beada8e11b6d060b5d9cde4a
Ryan Murphy <ryan.murphy@rychannel.com>
2026-01-09 03:08:49 +0000

    Update Dockerfile and devcontainer settings for Go environment; add initial Go program

---
+++ src/const/main.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func main() {
+	const pi = 3.14159
+
+	const {
+		red = iota + 1
+		yellow
+		green
+		brown
+		blue
+		pink
+		black
+	}
+
+	fmt.Printf("Red: %v points \n", red)
+	fmt.Printf("Blue: %v points \n", blue)
+	fmt.Printf("Black: %v points \n", black)
+}
